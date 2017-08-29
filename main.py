a2b59456d668573fc16370efd8df8ce7799e9a30
Pannawit Samatthiyadikun <s.pannawit@gmail.com>
2017-08-30 04:40:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 MNIST Handwritten Digit Classifier with Neural Network
 ===============================================================================
 Implementation of multi-layer neural network with stochastic gradient descent
-(SGD) optimization method includingparameters for learning rate and
+(SGD) optimization method including parameters for learning rate and
 regularization.
 
 Other functions are grouped in class instead of separated into files for
